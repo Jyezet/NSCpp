44bca95f16b47472b7cd25a9435270c4f45719bc
Jyezet <65241791+Jyezet@users.noreply.github.com>
2023-10-07 22:37:46 -0300

    Moved definitions to NSCpp namespace

--- include/definitions.h
+++ include/definitions.h
@@ -1,42 +1,44 @@
 #pragma once
 #include <iostream>
 
-// Why nationstates, why
-enum class DispatchCategory {
-	Factbook = 1,
-	Bulletin = 3,
-	Account = 5,
-	Meta = 8
-};
+namespace NSCpp {
+	// Why nationstates, why
+	enum class DispatchCategory {
+		Factbook = 1,
+		Bulletin = 3,
+		Account = 5,
+		Meta = 8
+	};
 
-enum class DispatchSubcategory {
-	Overview = 100,
-	History = 101,
-	Geography = 102,
-	FactCulture = 103,
-	Politics = 104,
-	Legislation = 105,
-	Religion = 106,
-	FactMilitary = 107,
-	Economy = 108,
-	International = 109,
-	Trivia = 110,
-	Miscellaneous = 111,
-	Policy = 305,
-	News = 315,
-	Opinion = 325,
-	Campaign = 385,
-	AccMilitary = 505,
-	Trade = 515,
-	Sport = 525,
-	Drama = 535,
-	Diplomacy = 545,
-	Science = 555,
-	AccCulture = 565,
-	Other = 595,
-	Gameplay = 835,
-	Reference = 845
-};
+	enum class DispatchSubcategory {
+		Overview = 100,
+		History = 101,
+		Geography = 102,
+		FactCulture = 103,
+		Politics = 104,
+		Legislation = 105,
+		Religion = 106,
+		FactMilitary = 107,
+		Economy = 108,
+		International = 109,
+		Trivia = 110,
+		Miscellaneous = 111,
+		Policy = 305,
+		News = 315,
+		Opinion = 325,
+		Campaign = 385,
+		AccMilitary = 505,
+		Trade = 515,
+		Sport = 525,
+		Drama = 535,
+		Diplomacy = 545,
+		Science = 555,
+		AccCulture = 565,
+		Other = 595,
+		Gameplay = 835,
+		Reference = 845
+	};
 
-const std::string validNationShards[] = { "ADMIRABLE", "ADMIRABLES", "ANIMAL", "ANIMALTRAIT", "ANSWERED", "BANNER", "BANNERS", "CAPITAL", "CATEGORY", "CENSUS", "CRIME", "CURRENCY", "CUSTOMLEADER", "CUSTOMCAPITAL", "CUSTOMRELIGION", "DBID", "DEATHS", "DEMONYM", "DEMONYM2", "DEMONYM2PLURAL", "DISPATCHES", "DISPATCHLIST", "ENDORSEMENTS", "FACTBOOKS", "FACTBOOKLIST", "FIRSTLOGIN", "FLAG", "FOUNDED", "FOUNDEDTIME", "FREEDOM", "FULLNAME", "GAVOTE", "GDP", "GOVT", "GOVTDESC", "GOVTPRIORITY", "HAPPENINGS", "INCOME", "INDUSTRYDESC", "INFLUENCE", "LASTACTIVITY", "LASTLOGIN", "LEADER", "LEGISLATION", "MAJORINDUSTRY", "MOTTO", "NAME", "NOTABLE", "NOTABLES", "POLICIES", "POOREST", "POPULATION", "PUBLICSECTOR", "RCENSUS", "REGION", "RELIGION", "RICHEST", "SCVOTE", "SECTORS", "SENSIBILITIES", "TAX", "TGCANRECRUIT", "TGCANCAMPAIGN", "TYPE", "WA", "WABADGES", "WCENSUS", "ZOMBIE" };
-const std::string privateShards[] = { "DOSSIER", "ISSUES", "ISSUESUMMARY", "NEXTISSUE", "NEXTISSUETIME", "NOTICES", "PACKS", "PING", "RDOSSIER", "UNREAD" };
+	const std::string validNationShards[] = { "ADMIRABLE", "ADMIRABLES", "ANIMAL", "ANIMALTRAIT", "ANSWERED", "BANNER", "BANNERS", "CAPITAL", "CATEGORY", "CENSUS", "CRIME", "CURRENCY", "CUSTOMLEADER", "CUSTOMCAPITAL", "CUSTOMRELIGION", "DBID", "DEATHS", "DEMONYM", "DEMONYM2", "DEMONYM2PLURAL", "DISPATCHES", "DISPATCHLIST", "ENDORSEMENTS", "FACTBOOKS", "FACTBOOKLIST", "FIRSTLOGIN", "FLAG", "FOUNDED", "FOUNDEDTIME", "FREEDOM", "FULLNAME", "GAVOTE", "GDP", "GOVT", "GOVTDESC", "GOVTPRIORITY", "HAPPENINGS", "INCOME", "INDUSTRYDESC", "INFLUENCE", "LASTACTIVITY", "LASTLOGIN", "LEADER", "LEGISLATION", "MAJORINDUSTRY", "MOTTO", "NAME", "NOTABLE", "NOTABLES", "POLICIES", "POOREST", "POPULATION", "PUBLICSECTOR", "RCENSUS", "REGION", "RELIGION", "RICHEST", "SCVOTE", "SECTORS", "SENSIBILITIES", "TAX", "TGCANRECRUIT", "TGCANCAMPAIGN", "TYPE", "WA", "WABADGES", "WCENSUS", "ZOMBIE", "DOSSIER", "ISSUES", "ISSUESUMMARY", "NEXTISSUE", "NEXTISSUETIME", "NOTICES", "PACKS", "PING", "RDOSSIER", "UNREAD" };
+	const std::string privateShards[] = { "DOSSIER", "ISSUES", "ISSUESUMMARY", "NEXTISSUE", "NEXTISSUETIME", "NOTICES", "PACKS", "PING", "RDOSSIER", "UNREAD" };
+}
